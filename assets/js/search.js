027c28dbd93060bcef1d0cf8afb14e933bdedec0
Satadeep Dasgupta <satadeep3927@gmail.com>
2021-11-01 13:07:26 +0530

    Add files via upload

---
+++ assets/js/search.js
@@ -0,0 +1,137 @@
+var Data =[
+    {
+        "song":"Deliverance",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    },
+    {
+        "song":"Adagio in C minor",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    },
+    {
+        "song":"Dance with Stranger",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    },
+    {
+        "song":"Love is all",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    },
+    {
+        "song":"Niki Nana(We're one)",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    }
+    ,
+    {
+        "song":"Renegade",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    }
+    ,
+    {
+        "song":"Southern Exposure",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    }
+    ,
+    {
+        "song":"Tribute",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    },
+    {
+        "song":"Waltz in 7/8",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    },
+    {
+        "song":"Nightingale",
+        "link":"index.html",
+        "album":"Tribute",
+        "image":"assets/img/demo/u1.jpg"
+    },
+    {
+        "song":"A love for life",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"Chasing Shadows",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"Flight of Fantacy",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"Marching Season",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"Nostalgia",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"Once upon a time",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"Reflections of passion",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"Santorini",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"Standing in motion",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"Swept Away",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"The Rain must fall",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+    {
+        "song":"To the One who knows",
+        "link":"theverybestofyanni.html",
+        "album":"The very best of Yanni",
+        "image":"assets/img/demo/a2.jpg"
+    },
+]
